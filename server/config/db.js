a95c23b0e85268606580c73d7fa23807d579dedb
Bob Streeter <streeterb@bell.net>
2023-04-22 20:32:42 -0400

    update

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
-const connectDB = async() => {
+let conn = null
+const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI)
-        console.log(`Database Connected: ${conn.connection.host}`)
+        if (!conn) {
+            const conn = await mongoose.connect(process.env.MONGODB_URI)
+            console.log(`Database Connected: ${conn.connection.host}`)
+        }
+
     } catch (error) {
         console.log(error)
     }
 }
+
 module.exports = connectDB
